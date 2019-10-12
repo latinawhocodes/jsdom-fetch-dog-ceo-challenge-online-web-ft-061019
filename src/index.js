538282c5b522cc00c56af973696ddf3718d0b45b
latinawhocodes <alyssaroque01@gmail.com>
2019-10-11 22:54:53 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", function(){
+    fetchImg()
+});
+
+function fetchImg(){
+    const imgUrl = 'https://dog.ceo/api/breeds/image/random/4'
+    fetch(imgUrl)
+        .then(response => response.json())
+        .then(json => addImageElements(json))
+}
+
+function addImageElements(json){
+    const imgLink = json.message
+    const imgDiv = document.querySelector('#dog-image-container')
+
+    imgLink.forEach(link => {
+        const img = document.createElement("img")
+        img.src = link
+        img.alt = "dog ceo image"
+        imgDiv.appendChild(img)
+    })
+}
+
+function fetchBreeds(){
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    fetch(breedUrl)
+        .then(response => response.json())
+        .then(json => addBreeds(json))
+}
+
+function addBreeds(json){
+    const breed = Object.entries(json.message)
+    const breedUl = document.querySelector('#dog-breeds')
+
+    breed.forEach(breed => {
+        const liElement = document.createElement("li")
+        liElement.textContent = breed
+        breedUl.appendChild(liElement)
+
+    })
+}
+
+function changeColor(event){
+    event.target.style.color = 'pink';
+}
